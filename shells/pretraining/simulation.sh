8e8306b8ec3b493bfc630c9a33c164ec3d198f8d
mgiberna <marco.giberna@uni.lu>
2024-02-26 11:25:55 +0100

    testing

--- shells/pretraining/simulation.sh
+++ shells/pretraining/simulation.sh
@@ -5,8 +5,8 @@ python train.py \
   --lr 1e-4 --decay_step 4 8 --decay_gamma 0.5 --lambda_opacity 1e-5 \
   --exp_name nof_simulation
 
-python eval.py \
-  --root_dir /home/marco/catkin_ws/src/ir-mcl/data/simulation/ \
-  --ckpt_path ./logs/nof_simulation/version_0/checkpoints/best.ckpt \
-  --N_samples 256 --chunk 92160 --perturb 0 --noise_std 0 --L_pos 10 \
-  --feature_size 256 --use_skip
+# python eval.py \
+#   --root_dir /home/marco/catkin_ws/src/ir-mcl/data/simulation/ \
+#   --ckpt_path ./logs/nof_simulation/version_0/checkpoints/best.ckpt \
+#   --N_samples 256 --chunk 92160 --perturb 0 --noise_std 0 --L_pos 10 \
+#   --feature_size 256 --use_skip
